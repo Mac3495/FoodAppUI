da72de1e4003f883f2d0ca247c357b45792332e8
nubol23 <rafael.rvp98@gmail.com>
2019-05-19 19:38:57 -0400

    Added profile information layout

---
+++ app/src/main/java/com/project/rafa/yourfood/ui/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.project.rafa.yourfood.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.project.rafa.yourfood.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
